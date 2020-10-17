1249fec2fea6f2b9b471a7e4044dd4f3fb25c102
Michael Li <michael.yifan.li@gmail.com>
2020-10-17 10:12:32 -0400

    first dotfiles yay

---
+++ bash/aliases.sh
@@ -0,0 +1,9 @@
+alias ml="cd /mnt/c/Users/micha/OneDrive/Documents/Projects/Learning/MachineLearning"
+alias rsch="cd /mnt/c/Users/micha/OneDrive/Documents/Projects/Research"
+alias proj="cd /mnt/c/Users/micha/OneDrive/Documents/Projects"
+
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+
